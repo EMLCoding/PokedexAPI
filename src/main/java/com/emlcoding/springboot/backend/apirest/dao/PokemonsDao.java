d1c17fdbdb5d041b1ad0c0abee65cebc288efabf
Edu-Estudios <eduardo.ml.trabajo@gmail.com>
2022-08-17 17:29:02 +0200

    Cambios 17-08

--- src/main/java/com/emlcoding/springboot/backend/apirest/dao/PokemonsDao.java
+++ src/main/java/com/emlcoding/springboot/backend/apirest/dao/PokemonsDao.java
@@ -1,20 +1,19 @@
 package com.emlcoding.springboot.backend.apirest.dao;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import java.util.List;
 
 import com.emlcoding.springboot.backend.apirest.entity.Pokemon;
-
 
 public interface PokemonsDao extends JpaRepository<Pokemon, Long> {
 
 	@Query("SELECT p FROM Pokemon p ORDER BY p.pokedexNum")
 	Page<Pokemon> findAllPaginable(Pageable pageable);
-	
+
 	@Query(value = "SELECT p.* FROM pokemons as p LEFT JOIN pokemon_favorites pf ON p.id = pf.pokemon_id WHERE pf.user_id = ?1", nativeQuery = true)
 	List<Pokemon> getFavoritesBy(Long userId);
 }
-
